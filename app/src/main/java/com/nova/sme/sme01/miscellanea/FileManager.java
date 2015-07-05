eebbd8f3f34ee803684c90982b7be2dd656395a3
nick756 <gundartsev-v@yandex.ru>
2015-07-05 11:30:35 +0300

    First commit

---
+++ app/src/main/java/com/nova/sme/sme01/miscellanea/FileManager.java
@@ -0,0 +1,13 @@
+package com.nova.sme.sme01.miscellanea;
+
+/*
+ **************************************************
+ *                                                *
+ *   Read/Write from/to file                      *
+ *                                                *
+ **************************************************
+ */
+
+public class FileManager {
+
+}
