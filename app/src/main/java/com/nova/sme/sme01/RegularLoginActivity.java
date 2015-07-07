e455675a49e75cd1b687d34d56dfd4a11af6836e
nick756 <gundartsev-v@yandex.ru>
2015-07-08 00:27:01 +0300

    First commit

--- app/src/main/java/com/nova/sme/sme01/RegularLoginActivity.java
+++ app/src/main/java/com/nova/sme/sme01/RegularLoginActivity.java
@@ -2,16 +2,53 @@ package com.nova.sme.sme01;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
 
-public class RegularLoginActivity extends ActionBarActivity {
+public class RegularLoginActivity extends AppCompatActivity {
+    private TextView                      user;
+    private TextView                      company_name;
+    private TextView                      originator;
+    private TextView                      role;
+    private android.widget.RelativeLayout base_layout;
+    private FormResizing                  FR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regular_login);
+
+        CommonClass c_c = (CommonClass)getIntent().getSerializableExtra(MainActivity.MAIN_INFO);
+
+        base_layout  = (android.widget.RelativeLayout) findViewById(R.id.base_layout_reg);
+        user         = (TextView) findViewById(R.id.reg_user_name_id);
+        company_name = (TextView) findViewById(R.id.reg_company_name_id);
+        originator   = (TextView) findViewById(R.id.reg_originator_id);
+        role         = (TextView) findViewById(R.id.reg_role_id);
+
+        user.setText(c_c.name);
+        company_name.setText(c_c.company);
+        originator.setText(c_c.originator);
+        role.setText(c_c.role);
+
+        FR = new FormResizing(this, base_layout);
+
+        ViewTreeObserver vto = base_layout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+                base_layout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                FR.resize_n();
+            }
+        });
+
     }
 
     @Override
@@ -20,6 +57,11 @@ public class RegularLoginActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_regular_login, menu);
         return true;
     }
+
+    public void clickButton(View v) {
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
