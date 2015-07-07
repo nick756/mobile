c684ed625a37a086c2ca534b3b5b7fd58371a529
nick756 <gundartsev-v@yandex.ru>
2015-07-07 10:18:30 +0300

    First commit

---
+++ app/src/main/java/com/nova/sme/sme01/FirstTimeLoginActivity.java
@@ -0,0 +1,50 @@
+package com.nova.sme.sme01;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class FirstTimeLoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first_time_login, menu);
+        return true;
+    }
+
+    public void clickSynchronizeButton(View v) {
+
+    }
+    public void clickLockButton(View v) {
+
+    }
+    public void clickLogoutButton(View v) {
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
