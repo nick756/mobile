93c323395d5674e7352345c612a2050088ab4542
nick756 <gundartsev-v@yandex.ru>
2015-07-09 11:24:41 +0300

    First commit

--- app/src/main/java/com/nova/sme/sme01/transactions/GetOperations.java
+++ app/src/main/java/com/nova/sme/sme01/transactions/GetOperations.java
@@ -14,10 +14,12 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Root(name="result")
-public class GetOperations {
+public class GetOperations implements Serializable {
 
     @Attribute
     private String code;
@@ -27,7 +29,7 @@ public class GetOperations {
     private String id;
     public  String getId() {return this.id;}
 
-    @Element(required = false, name="orginator")
+    @Element(required = false, name="originator")
     private String originator;
     public  String getOriginator(){return this.originator;}
 
@@ -36,11 +38,120 @@ public class GetOperations {
     public  String getDescription(){return this.description;}
 
     @ElementList(required=false, name="supportedOperations")
-    private List<Operation> list;
-    public List<Operation> getOperationsList(){return this.list;}
+    private ArrayList<Operation> list;
+    public  ArrayList<Operation> getOperationsList(){return this.list;}
 
-//    @Element(required=false, name="supportedOperations")
-//    SupportedOperations supportedOperations;
-//    public SupportedOperations getSupportedOpeartions(){return this.supportedOperations;}
+    public GetOperations(){}
 
+    public GetOperations(GetOperations obj) {
+        this.code        = obj.getCode();
+        this.id          = obj.getId();
+        this.originator  = obj.getOriginator();
+        this.description = obj.getDescription();
+
+        this.list = new ArrayList<Operation>(obj.getOperationsList());
+    }
+
+    public boolean equals(GetOperations obj) {
+        if (!this.code.equals(obj.getCode()))
+            return false;
+
+        if (!this.id.equals(obj.getId()))
+            return false;
+
+        if(!this.originator.equals(obj.getOriginator()))
+            return false;
+
+        if (!this.description.equals(obj.getDescription()))
+            return false;
+
+
+        ArrayList<Operation> olist = obj.getOperationsList();
+        if (list == null && olist != null)
+            return false;
+        if (list != null && olist == null)
+            return false;
+
+        if (list != null && olist != null) {
+            if (list.size() != olist.size())
+                return false;
+
+            for (int i = 0; i < list.size(); i++)
+                if (!list.get(i).equals(olist.get(i)))
+                    return false;
+        }
+
+        return true;
+    }
+
+/*
+    public String getSerializedString() {
+        String    str = new String();
+        Operation operation;
+        int       size;
+
+        if (list != null) {
+            if ((size = list.size()) > 0) {
+                for(int i = 0; i < size; i ++) {
+                    operation = list.get(i);
+                    str += "code="     + operation.getCode()     + ";";
+                    str += "name="     + operation.getName()     + ";";
+                    str += "inbound="  + operation.getInbound()  + ";";
+                    str += "outbound=" + operation.getOutbound() + ";";
+                    str += "type="     + operation.getType()     + ";";
+               }
+            }
+        }
+        return str;
+    }
+
+    public List<Operation> getDeserialized(String str) {
+        String          s;
+        List<Operation> list = null;
+        Operation       operation;
+        int             pos, pos1, counter = 0, cnt;
+        if (str.length() > 0) {
+            while(true) {
+                if ((++counter) > 1000) {
+                    // error something wrong with string
+                    return null;
+                }
+                pos       = str.indexOf(";");
+                if (pos == -1)
+                    break;
+
+                operation = new Operation();
+                cnt = 0;
+                while(true) {
+                    if ((++cnt) > 1000) {
+                        // error something wrong with string
+                        return null;
+                    }
+                    if (str.indexOf("code=") == 0) {
+                        operation.setCode(str.substring(5, pos - 1));
+                    } else if (str.indexOf("name=") == 0) {
+                        operation.setName(str.substring(5, pos - 1));
+                    } else if (str.indexOf("inbound=") == 0) {
+                        operation.setInbound(str.substring(8, pos - 1));
+                    } else if (str.indexOf("outbound=") == 0) {
+                        operation.setOutbound(str.substring(9, pos - 1));
+                    } else if (str.indexOf("type=") == 0) {
+                        operation.setType(str.substring(5, pos - 1));
+                        list.add(operation);
+                        break;
+                    } else {
+                        break;
+                    }
+                    str = str.substring(pos + 1);
+                }
+
+
+//                str = str.substring(pos + 1);
+                if (str.length() == 0)
+                    break;
+            }
+        }
+        return list;
+    }
+    */
 }
--- app/src/main/java/com/nova/sme/sme01/transactions/Operation.java
+++ app/src/main/java/com/nova/sme/sme01/transactions/Operation.java
@@ -12,26 +12,69 @@ package com.nova.sme.sme01.transactions;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
+import java.io.Serializable;
+
 @Root(name="operation")
-public class Operation {
+public class Operation implements Serializable {
 
-    @Element(required=false, name = "code")
+    @Element(required = false, name = "code")
     private String code;
-    public String getCode() {return code;}
+    public String getCode() {
+        return code;
+    }
+    public void   setCode(String code) {this.code = code;}
 
-    @Element(required=false, name = "name")
+    @Element(required = false, name = "name")
     private String name;
-    public String getName() {return name;}
+    public String getName() {
+        return name;
+    }
+    public void   setName(String name) {this.name = name;}
 
-    @Element(required=false, name = "inbound")
+    @Element(required = false, name = "inbound")
     private String inbound;
-    public String getInbound() {return inbound;}
+    public String getInbound() {
+        return inbound;
+    }
+    public void   setInbound(String inbound) {this.inbound = inbound;}
 
-    @Element(required=false, name = "outbound")
+    @Element(required = false, name = "outbound")
     private String outbound;
-    public String getOutbound() {return outbound;}
+    public String getOutbound() {
+        return outbound;
+    }
+    public void   setOutbound(String outbound) {this.outbound = outbound;}
 
-    @Element(required=false, name = "type")
+    @Element(required = false, name = "type")
     private String type;
-    public String getType() {return type;}
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {this.type = type;}
+
+
+    public boolean equals(Operation obj) {
+        if (!this.code.equals(obj.getCode()))
+            return false;
+        if (!this.name.equals(obj.getName()))
+            return false;
+        if (!this.inbound.equals(obj.getInbound()))
+            return false;
+        if(!this.outbound.equals(obj.getOutbound()))
+            return false;
+        return this.type.equals(obj.getType());
+    }
+
+
+    Operation() {
+
+    }
+
+    Operation(Operation obj) {
+        this.code     = obj.getCode();
+        this.name     = obj.getName();
+        this.inbound  = obj.getInbound();
+        this.outbound = obj.getOutbound();
+        this.type     = obj.getType();
+    }
 }
