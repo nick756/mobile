7ab3cb19a151a46e8a217d6ed726a9189ec2cb0b
nick756 <gundartsev-v@yandex.ru>
2015-07-16 10:25:40 +0300

    First commit

---
+++ app/src/main/java/com/nova/sme/sme01/xml_reader_classes/TransactionXML.java
@@ -0,0 +1,12 @@
+package com.nova.sme.sme01.xml_reader_classes;
+
+
+import org.simpleframework.xml.Element;
+
+public class TransactionXML extends BaseXML {
+
+    @Element(required = false, name = "transactionID")
+    private String transactionID;
+    public  String gettransactionId() {return transactionID;}
+
+}
