a44d36d328c265e38b80cbe71ab5d7e320f71627
nick756 <gundartsev-v@yandex.ru>
2015-07-28 19:46:48 +0300

    First commit

--- app/src/main/java/com/nova/sme/sme01/miscellanea/FillWithTransactionsList.java
+++ app/src/main/java/com/nova/sme/sme01/miscellanea/FillWithTransactionsList.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -65,7 +66,7 @@ public class FillWithTransactionsList {
 
             ViewGroup.MarginLayoutParams params;
             for (int i = 0; i < list.size(); i++) {
-                RelativeLayout ll = (RelativeLayout) inflater.inflate(R.layout.transaction_view_item, null);
+                LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.tarnsaction_view_item_n, null);
 
                 sv.addView(ll);
                 setValues(ll, list.get(i));
@@ -74,7 +75,7 @@ public class FillWithTransactionsList {
                 params.bottomMargin = 5;
             }
             return true;
-        } catch (Exception err) {
+        } catch (Exception err) {//java.lang.ClassCastException: android.widget.LinearLayout cannot be cast to android.widget.RelativeLayout
             println(err.getMessage().toString());
         }
         return false;
@@ -82,34 +83,66 @@ public class FillWithTransactionsList {
     public float pxToDp(float px) {
         return px / activity.getResources().getDisplayMetrics().density;
     }
-    private void setValues(RelativeLayout layout, Record record) {
-        View           view;
-        String         tag;
-        TextView       text;
+    private void setValues(LinearLayout layout, Record record) {
+        View           view = null;
+        String         tag  = "";
+        String         type = null;
+        TextView       text = null;
+        LinearLayout   inner_layout = null;
 
-         for (int j = 0; j < layout.getChildCount(); j ++) {
-            view      = layout.getChildAt(j);
-            tag       = (String) view.getTag();
+         for (int i = 0; i < layout.getChildCount(); i ++) {
+            inner_layout = (LinearLayout) layout.getChildAt(i);
+            tag          = (String)inner_layout.getTag();
+            for (int j = 0; j < inner_layout.getChildCount(); j ++) {
+                view = inner_layout.getChildAt(j);
+                tag = (String) view.getTag();
 
+                if (tag != null) {
+                    if (tag.equals("code")) {
+                        text = (TextView) view;
+                        text.setText(record.getTranCode());
+                    } else if (tag.equals("date")) {
+                        text = (TextView) view;
+                        text.setText(record.getDate());
+                    } else if (tag.equals("type")) {
+                        type = record.getType().trim();
+                        text = (TextView) view;
+
+                        if (type.indexOf("IN:") == 0) {
+                            setTypeIcon(inner_layout, R.mipmap.ic_in_bound);
+                            type = type.substring(3);
+                        } else {
+                            setTypeIcon(inner_layout, R.mipmap.ic_out_bound);
+                            type = type.substring(4);
+                        }
+                        text.setText(type);
+                    } else if (tag.equals("amount")) {
+                        text = (TextView) view;
+                        text.setText(record.getAmount());
+                    } else if (tag.equals("descr")) {
+                        text = (TextView) view;
+                        text.setText(record.getDescr());
+                    } else if (tag.equals("operator")) {
+                        text = (TextView) view;
+                        text.setText(record.getOperator());
+                    }
+                }
+            }
+        }
+    }
+    private void setTypeIcon(LinearLayout   inner_layout, int id) {
+        int    cnt = inner_layout.getChildCount();
+        View   view;
+        String tag;
+        ImageView img;
+        for (int  i = 0; i < cnt; i ++) {
+            view = inner_layout.getChildAt(i);
+            tag  = (String)view.getTag();
             if (tag != null) {
-                if (tag.equals("code")) {
-                    text = (TextView) view;
-                    text.setText(record.getTranCode());
-                } else if (tag.equals("date")) {
-                    text = (TextView) view;
-                    text.setText(record.getDate());
-                } else if (tag.equals("type")) {
-                    text = (TextView) view;
-                    text.setText(record.getType());
-                } else if (tag.equals("amount")) {
-                    text = (TextView) view;
-                    text.setText(record.getAmount());
-                } else if (tag.equals("descr")) {
-                    text = (TextView) view;
-                    text.setText(record.getDescr());
-                } else if (tag.equals("operator")) {
-                    text = (TextView) view;
-                    text.setText(record.getOperator());
+                if (tag.equals("in_or_out")) {
+                    img = (ImageView) view;
+                    img.setImageResource(id);
+                    return;
                 }
             }
         }
